16e4e51ac0957eafe5ca203fe98cb308a90a1783
Jovan Jevtic <jovanjevtic132@gmail.com>
2021-08-12 04:07:12 +0200

    Setup

---
+++ src/StocksNow/StocksNow.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+interface Props {
+
+}
+
+const StocksNow: React.FC<Props> = () => {
+    return(
+        <div className="app">
+            <h1>Stocks Now</h1>
+        </div>
+    );
+}
+
+export default StocksNow;
